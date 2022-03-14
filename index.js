cd6aa2d364ca2ad6b68fac5ac535237fec93ca64
CaptM44 <mmorgan4x@gmail.com>
2022-03-14 16:29:44 -0400

    navbar and filter updates

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-//// @ts-check
+// @ts-check
  /** @type {import("jsZip")} */ var JSZip;
  /** @type {import("highcharts")} */ var Highcharts;
- /** @type {import("jquery")} */ var $;
+ /** @type {import("moment")} */ var moment;
+
+let data = [];
 
 async function uploadFile(file) {
     $('#loading').removeClass('hide');
 
-    let data = await parseZip(file);
-
+    data = await parseZip(file);
     data = data.filter(t => !!t.Date && !!t.Time && !!t['avg(temp)'] && !!t['avg(humidity)']);
-    console.log(data)
     data = data.map(t => ({
         date: new Date(`${t.Date} ${t.Time}:00`).getTime(),
-        temp: toF(+t['avg(temp)']),
+        temp: +t['avg(temp)'],
         humidity: +t['avg(humidity)']
     }));
     data.sort((a, b) => a.date - b.date);
-    data = data.slice(0, 10000)
 
-    drawChart(data);
+    update();
 }
 
 async function parseZip(file) {
@@ -27,13 +26,13 @@ async function parseZip(file) {
     let files = Object.values(zip.files);
     let csvs = files.filter(t => t.name.endsWith('.csv'));
 
-    let data = [];
+    let d = [];
     for (let csv of csvs) {
         let csvStr = await csv.async('text');
         let rows = parseCSV(csvStr);
-        data = data.concat(rows);
+        d = d.concat(rows);
     }
-    return data;
+    return d;
 }
 
 function parseCSV(csvStr) {
@@ -41,10 +40,10 @@ function parseCSV(csvStr) {
     let arr = csvStr.split('\n');
     let headers = arr[0].split(',');
     for (var i = 1; i < arr.length; i++) {
-        let data = arr[i].split(',');
+        let d = arr[i].split(',');
         let obj = {};
-        for (let j = 0; j < data.length; j++) {
-            obj[headers[j].trim()] = data[j].trim();
+        for (let j = 0; j < d.length; j++) {
+            obj[headers[j].trim()] = d[j].trim();
         }
         rows.push(obj);
     }
@@ -52,20 +51,58 @@ function parseCSV(csvStr) {
 }
 
 function toF(celcius) {
-    return (celcius * 9 / 5) + 32;
+    return;
 }
 
-function drawChart(data) {
-
+function update() {
     $('#landing').addClass('hide');
     $('#chart').removeClass('hide');
+    $('.navbar-nav').removeClass('hide');
+
+    //units
+    let isCelcius = $('#degree').val() == 'C';
+    let filteredData = data.map(t => ({ date: t.date, temp: isCelcius ? t.temp : (t.temp * 9 / 5) + 32, humidity: t.humidity }));
+
+    //date range
+    let maxDate = moment(data[data.length - 1].date);
+    let minDate = moment(data[0].date);
+
+    let filterEnd = $('#endDate').val() ? moment($('#endDate').val()) : null;
+    let filterStart = $('#startDate').val() ? moment($('#startDate').val()) : null
+    if (!filterEnd) { filterEnd = maxDate }
+    if (!filterStart) { filterStart = minDate }
+
+    $('#endDate').val(filterEnd.format('YYYY-MM-DD'));
+    $('#startDate').val(filterStart.format('YYYY-MM-DD'));
+
+    filteredData = filteredData.filter(t => t.date > filterStart.valueOf() && t.date < filterEnd.valueOf());
+
+    //data chunking
+    let increment = +$('#increment').val();
+    let chunkData = [];
+    for (let i = 0; i < filteredData.length; i += increment) {
+        let chunk = filteredData.slice(i, i + increment);
+        chunkData.push({
+            date: chunk[0].date,
+            temp: chunk.reduce((a, b) => a + b.temp, 0) / chunk.length,
+            humidity: chunk.reduce((a, b) => a + b.humidity, 0) / chunk.length
+        });
+    }
+    filteredData = chunkData;
+
+    drawChart(filteredData, () => $('#loading').addClass('hide'));
+}
+
+function drawChart(filteredData, cb) {
 
     let plotlines = [];
-    let start = new Date(data[0].date)
-    start = new Date(start.getFullYear(), start.getMonth(), start.getDate(), 0, 0, 0);
-    let end = new Date(data[data.length - 1].date);
-    for (let i = start.getTime(); i < end.getTime(); i += 86400000) {
-        plotlines.push(i)
+    if (filteredData.length) {
+        let start = new Date(filteredData[0].date)
+        start = new Date(start.getFullYear(), start.getMonth(), start.getDate(), 0, 0, 0);
+        let end = new Date(filteredData[filteredData.length - 1].date);
+        for (let i = start.getTime(); i < end.getTime(); i += 86400000) {
+            plotlines.push(i)
+        }
     }
 
     Highcharts.chart('chart', {
@@ -75,16 +112,14 @@ function drawChart(data) {
             type: 'line',
             panning: true,
             panKey: 'shift',
-            events: {
-                load: e => $('#loading').addClass('hide')
-            }
+            events: { load: cb }
         },
         title: false,
         subtitle: false,
         colors: ['#db3236', '#f4c20d', '#4885ed ', , '#3cba54 '],
         xAxis: {
             type: 'datetime',
-            plotLines: plotlines.map(t => ({ value: t }))
+            plotLines: plotlines.map(t => ({ value: t, color: '#eee' }))
         },
         yAxis: {
             min: 0,
@@ -99,12 +134,12 @@ function drawChart(data) {
 
         series: [{
             name: 'Temperature',
-            data: data.map(t => ([t.date, t.temp])),
-            tooltip: { valueSuffix: '&degF' },
+            data: filteredData.map(t => ([t.date, t.temp])),
+            tooltip: { valueSuffix: `&deg${$('#degree').val()}` },
         },
         {
             name: 'Humidity',
-            data: data.map(t => ([t.date, t.humidity])),
+            data: filteredData.map(t => ([t.date, t.humidity])),
             tooltip: { valueSuffix: '%' },
         }]
     });
@@ -136,6 +171,14 @@ function init() {
     $('#file-input').on('change', function (e) {
         uploadFile(this.files[0]);
     });
+
+    // filter inputs
+    $('#endDate').val(moment().format('YYYY-MM-DD'));
+    $('#startDate').val(moment().subtract(1, 'M').format('YYYY-MM-DD'))
+    $('input[type="date"]').on('mousedown', function (e) { e.preventDefault(); });
+    $('input[type="date"]').on('change', function () { update() });
+    $('select').on('change', function () { update() });
+
 }
 
 init();
